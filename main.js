d23072e8a692fb8ac666d1bb462e7987a5de4537
Abdalla <anddragon34@gmail.com>
2025-06-07 20:59:16 +0300

    center

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const apiKey="3fd22816468b4171213810732d6b90cf";
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
 
-async function checkWeather(){
-    const respose = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBox = document.querySelector(".search button");
+
+async function checkWeather(city){
+    const respose = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await respose.json();
 
     console.log(data);
@@ -11,5 +14,11 @@ async function checkWeather(){
     document.querySelector(".humidity").innerText = data.main.humidity + "%";
     document.querySelector(".wind").innerText = data.wind.speed + "km/h";
 }
+
+searchBox.addEventListener(click, ()=>{
+
+    checkWeather(searchBox.value);
+    searchBox.value = "";
+})
 
 checkWeather();
